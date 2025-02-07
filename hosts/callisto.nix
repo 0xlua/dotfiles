da9a7fab3bb2cb5322fd2dd60180035b42d797ee
0xlua <dev@lukasjordan.com>
2025-02-07 09:34:16 +0100

    feat: show max 10 boot entries

--- hosts/callisto.nix
+++ hosts/callisto.nix
@@ -2,6 +2,8 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
+
   networking.hostName = "callisto"; # Define your hostname.
 
   networking.networkmanager.enable = true; # Enable networking
