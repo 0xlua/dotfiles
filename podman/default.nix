e490fa3f37a0ded20294ee8282d9c0a82831d475
0xlua <dev@lukasjordan.com>
2025-02-07 09:33:11 +0100

    feat: podman prune volumes

--- podman/default.nix
+++ podman/default.nix
@@ -20,6 +20,7 @@
     podman.enable = true;
     podman.defaultNetwork.settings.dns_enabled = true;
     podman.autoPrune.enable = true;
+    podman.autoPrune.flags = ["--all"];
     oci-containers.backend = "podman";
   };
 }
