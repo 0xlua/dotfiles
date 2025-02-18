bbc6bd7e5a7c12b00de8797e07ca780b77f7f9a3
0xlua <dev@lukasjordan.com>
2025-02-18 22:47:06 +0100

    fix: disable hl for now

--- home/server.home.nix
+++ home/server.home.nix
@@ -1,14 +1,13 @@
 {inputs, ...}: {
   imports = [
     inputs.home-manager.nixosModules.home-manager
-    inputs.hl
   ];
   home-manager.users.lua = {pkgs, ...}: {
     home = {
       packages = with pkgs; [
         tailspin
         # TODO: use ${system}
-        hl.packages."x86_64-linux"
+        # inputs.hl.packages."x86_64-linux".default
         podman-tui
       ];
     };
