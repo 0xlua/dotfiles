3a1c8e00885372991d91c9f13e487df371febe31
0xlua <dev@lukasjordan.com>
2025-02-23 19:35:39 +0100

    feat: add paperless-ngx consume share

--- graphical.nix
+++ graphical.nix
@@ -36,6 +36,14 @@
     in ["${automount_opts},credentials=${config.sops.templates."smb-secrets".path},uid=1000,gid=100"];
   };
 
+  fileSystems."/home/lua/paperless_inbox" = {
+    device = "//io.internal/scanner";
+    fsType = "cifs";
+    options = let
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in ["${automount_opts},credentials=${config.sops.templates."smb-secrets".path},uid=1000,gid=100"];
+  };
+
   security.rtkit.enable = true;
 
   services.pipewire = {
