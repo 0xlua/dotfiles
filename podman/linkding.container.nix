400bd7e12a4b7d4bacd087b384f1dc43f780f30b
0xlua <dev@lukasjordan.com>
2025-02-26 16:26:02 +0100

    chore: move linkding oidc to rauthy

--- podman/linkding.container.nix
+++ podman/linkding.container.nix
@@ -1,4 +1,8 @@
-{...}: {
+{config, ...}: {
+  sops.secrets.linkdingOidcSecret = {};
+  sops.templates."linkding-env".content = ''
+    OIDC_RP_CLIENT_SECRET=${config.sops.placeholder.linkdingOidcSecret}
+  '';
   virtualisation.oci-containers.containers.linkding = {
     image = "docker.io/sissbruecker/linkding:latest";
     autoStart = true;
@@ -6,13 +10,16 @@
       "io.containers.autoupdate" = "registry";
     };
     user = "1000:100";
+    environmentFiles = [
+      config.sops.templates."linkding-env".path
+    ];
     environment = {
       LD_SUPERUSER_NAME = "lua";
       LD_ENABLE_OIDC = "True";
-      OIDC_OP_AUTHORIZATION_ENDPOINT = "http://mail.lua.one/authorize/code";
-      OIDC_OP_TOKEN_ENDPOINT = "http://mail.lua.one/auth/token";
-      OIDC_OP_USER_ENDPOINT = "http://mail.lua.one/auth/userinfo";
-      OIDC_OP_JWKS_ENDPOINT = "http://mail.lua.one/auth/jwks.json";
+      OIDC_OP_AUTHORIZATION_ENDPOINT = "https://auth.lua.one/auth/v1/oidc/authorize";
+      OIDC_OP_TOKEN_ENDPOINT = "https://auth.lua.one/auth/v1/oidc/token";
+      OIDC_OP_USER_ENDPOINT = "https://auth.lua.one/auth/v1/oidc/userinfo";
+      OIDC_OP_JWKS_ENDPOINT = "https://auth.lua.one/auth/v1/oidc/certs";
       OIDC_RP_CLIENT_ID = "linkding";
     };
     volumes = [
