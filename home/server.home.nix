964469888c80452737b65b161aa01855185fd4a3
0xlua <dev@lukasjordan.com>
2025-03-05 12:39:12 +0100

    chore: disable tailspin, build failure

--- home/server.home.nix
+++ home/server.home.nix
@@ -5,7 +5,7 @@
   home-manager.users.lua = {pkgs, ...}: {
     home = {
       packages = with pkgs; [
-        tailspin
+        # tailspin
         # TODO: use ${system}
         # inputs.hl.packages."x86_64-linux".default
         podman-tui
