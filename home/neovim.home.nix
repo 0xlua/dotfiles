3c06b0d1da89698bdd66e2c14aa11ee378e7b7a8
0xlua <dev@lukasjordan.com>
2025-04-27 09:01:40 +0200

    feat(neovim): blink settings

--- home/neovim.home.nix
+++ home/neovim.home.nix
@@ -108,7 +108,9 @@
         blink-cmp = {
           enable = true;
           settings = {
+            signature.enabled = true;
             completion = {
+              menu.border = "rounded";
               documentation = {
                 auto_show = true;
                 auto_show_delay_ms = 200;
