4d7cfb9d40477e66b9dfe321566e117d98144c8e
0xlua <dev@lukasjordan.com>
2025-06-27 12:51:00 +0200

    chore: update rauthy

--- podman/rauthy.container.nix
+++ podman/rauthy.container.nix
@@ -1,42 +1,58 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   sops.secrets."rauthy/raft" = {};
   sops.secrets."rauthy/api" = {};
   sops.secrets."rauthy/active_key" = {};
   sops.secrets."rauthy/keys" = {};
   sops.secrets."rauthy/smtp/username" = {};
   sops.secrets."rauthy/smtp/password" = {};
-  sops.templates."rauthy-cfg" = {
+  sops.templates."rauthy-config-toml" = {
     mode = "0666";
-    content = ''
-      HQL_SECRET_RAFT=${config.sops.placeholder."rauthy/raft"}
-      HQL_SECRET_API=${config.sops.placeholder."rauthy/api"}
-      ENC_KEY_ACTIVE=${config.sops.placeholder."rauthy/active_key"}
-      ENC_KEYS=${config.sops.placeholder."rauthy/keys"}
-      SMTP_USERNAME=${config.sops.placeholder."rauthy/smtp/username"}
-      SMTP_PASSWORD="${config.sops.placeholder."rauthy/smtp/password"}"
-    '';
+    file = (pkgs.formats.toml {}).generate "rauthy-config" {
+      cluster = {
+        secret_raft = config.sops.placeholder."rauthy/raft";
+        secret_api = config.sops.placeholder."rauthy/api";
+        node_id = 1;
+        nodes = ["1 localhost:8100 localhost:8200"];
+      };
+
+      encryption = {
+        key_active = config.sops.placeholder."rauthy/active_key";
+        keys = [config.sops.placeholder."rauthy/keys"];
+      };
+
+      email = {
+        smtp_url = "mail.lua.one";
+        stmp_from = "Lua Account Service <auth@lua.one>";
+        smtp_username = config.sops.placeholder."rauthy/smtp/username";
+        smtp_password = config.sops.placeholder."rauthy/smtp/password";
+      };
+
+      webauthn = {
+        rp_id = "auth.lua.one";
+        rp_origin = "https://auth.lua.one:443";
+        rp_name = "Single Sign-On for lua.one";
+      };
+
+      server = {
+        proxy_mode = true;
+        trusted_proxies = ["10.88.0.0/16"];
+        pub_url = "auth.lua.one";
+        scheme = "http";
+      };
+    };
   };
   virtualisation.oci-containers.containers.rauthy = {
     image = "ghcr.io/sebadob/rauthy:latest";
     labels = {
       "io.containers.autoupdate" = "registry";
     };
-    environment = {
-      RP_ID = "auth.lua.one";
-      RP_ORIGIN = "https://auth.lua.one:443";
-      RP_NAME = "Single Sign-On for lua.one";
-      PROXY_MODE = "true";
-      TRUSTED_PROXIES = "10.88.0.0/16";
-      PUB_URL = "auth.lua.one";
-      HQL_NODE_ID = "1";
-      HQL_NODES = "1 localhost:8100 localhost:8200\n";
-      LISTEN_SCHEME = "http";
-      SMTP_URL = "mail.lua.one";
-      SMTP_FROM = "Lua Account Service <auth@lua.one>";
-    };
     volumes = [
       "/home/lua/podman/rauthy:/app/data"
-      "${config.sops.templates."rauthy-cfg".path}:/app/rauthy.cfg"
+      "${config.sops.templates."rauthy-config-toml".path}:/app/config.toml"
     ];
   };
 }
