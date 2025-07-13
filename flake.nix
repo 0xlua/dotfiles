5a6bfa511606f20b791f8f2d90dbde707d55f797
0xlua <dev@lukasjordan.com>
2025-07-13 10:48:22 +0200

    fix(ci): nixpkgs only follow cosmic

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS System Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
