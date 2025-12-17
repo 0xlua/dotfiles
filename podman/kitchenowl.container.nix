a92f0dc4c951603bfba8361dfb85d04d0da29de3
0xlua <dev@lukasjordan.com>
2025-12-17 23:02:15 +0100

    chore(oidc): move kitchenowl from rauthy to pocket-id

--- podman/kitchenowl.container.nix
+++ podman/kitchenowl.container.nix
@@ -1,8 +1,8 @@
 {config, ...}: {
-  sops.secrets.kitchenowlJwt = {
+  sops.secrets."kitchenowl/jwt_token" = {
     mode = "0440";
   };
-  sops.secrets.kitchenowlOidcSecret = {
+  sops.secrets."kitchenowl/oidc_client_secret" = {
     mode = "0440";
   };
 
@@ -15,15 +15,15 @@
     user = "1000:100";
     environment = {
       FRONT_URL = "https://kitchen.lua.one";
-      OIDC_ISSUER = "https://auth.lua.one/auth/v1";
-      OIDC_CLIENT_ID = "kitchenowl";
+      OIDC_ISSUER = "https://id.lua.one";
+      OIDC_CLIENT_ID = "9372f514-08ff-4e4e-ad52-ccded189e940";
       JWT_SECRET_KEY_FILE = "/run/secrets/jwt_secret_key";
       OIDC_CLIENT_SECRET_FILE = "/run/secrets/oidc_client_secret";
     };
     volumes = [
       "/home/lua/podman/kitchenowl:/data"
-      "${config.sops.secrets.kitchenowlJwt.path}:/run/secrets/jwt_secret_key:ro,U"
-      "${config.sops.secrets.kitchenowlOidcSecret.path}:/run/secrets/oidc_client_secret:ro,U"
+      "${config.sops.secrets."kitchenowl/jwt_token".path}:/run/secrets/jwt_secret_key:ro,U"
+      "${config.sops.secrets."kitchenowl/oidc_client_secret".path}:/run/secrets/oidc_client_secret:ro,U"
     ];
   };
 }
