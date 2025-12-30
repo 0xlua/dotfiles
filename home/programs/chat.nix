73a634e9f7f5c2f85e7fe52068bc6021ff96433d
0xlua <dev@lukasjordan.com>
2025-12-30 11:21:42 +0100

    fix: wrap all chat apps in option

--- home/programs/chat.nix
+++ home/programs/chat.nix
@@ -2,46 +2,49 @@
   config,
   inputs,
   pkgs,
+  lib,
   ...
 }: {
   imports = [
     inputs.sops-nix.homeManagerModules.sops
   ];
-  sops.secrets."liberaSasl".mode = "0440";
-  home.packages = with pkgs; [
-    rustdesk-flutter # remote desktop
-  ];
-  programs.gurk-rs = {
-    enable = config.home-modules.desktop.enable;
-    settings = {
-      signal_db_path = "${config.home.homeDirectory}/.local/share/gurk/signal-db";
-      first_name_only = false;
-      show_receipts = true;
-      notifications = true;
-      bell = true;
-      colored_messages = false;
-      default_keybindings = true;
-      user.name = "Lukas";
-      sqlite = {
-        url = "sqlite://${config.home.homeDirectory}/.local/share/gurk/gurk.sqlite";
-        _preserve_unencryped = false;
+  config = lib.mkIf config.home-modules.desktop.enable {
+    sops.secrets."liberaSasl".mode = "0440";
+    home.packages = with pkgs; [
+      rustdesk-flutter # remote desktop
+    ];
+    programs.gurk-rs = {
+      enable = true;
+      settings = {
+        signal_db_path = "${config.home.homeDirectory}/.local/share/gurk/signal-db";
+        first_name_only = false;
+        show_receipts = true;
+        notifications = true;
+        bell = true;
+        colored_messages = false;
+        default_keybindings = true;
+        user.name = "Lukas";
+        sqlite = {
+          url = "sqlite://${config.home.homeDirectory}/.local/share/gurk/gurk.sqlite";
+          _preserve_unencryped = false;
+        };
       };
     };
-  };
 
-  programs.halloy = {
-    enable = config.home-modules.desktop.enable;
-    settings = {
-      buffer.channel.topic = {
-        enabled = true;
-      };
-      servers.liberachat = {
-        channels = ["#voidlinux"];
-        nickname = "lua";
-        server = "irc.libera.chat";
-        sasl.plain = {
-          username = "lua";
-          password_file = config.sops.secrets."liberaSasl".path;
+    programs.halloy = {
+      enable = true;
+      settings = {
+        buffer.channel.topic = {
+          enabled = true;
+        };
+        servers.liberachat = {
+          channels = ["#voidlinux"];
+          nickname = "lua";
+          server = "irc.libera.chat";
+          sasl.plain = {
+            username = "lua";
+            password_file = config.sops.secrets."liberaSasl".path;
+          };
         };
       };
     };
