6fe4ec5792630ef4355cdbd2498c4f18e50cee18
0xlua <dev@lukasjordan.com>
2025-12-30 11:21:16 +0100

    fix: wrap android tools in option

--- home/development/android.nix
+++ home/development/android.nix
@@ -1,5 +1,16 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    scrcpy # mirror android screen
-  ];
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.home-modules.development.android;
+in {
+  options.home-modules.development.android.enable = lib.mkEnableOption "android dev tools";
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      scrcpy # mirror android screen
+    ];
+  };
 }
