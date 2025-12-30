cdf40082384b378d2b4b760ae4a4594d8eac16d8
0xlua <dev@lukasjordan.com>
2025-12-30 11:22:37 +0100

    feat: re-enable data & js dev tools on galileo

--- hosts/galileo/home.nix
+++ hosts/galileo/home.nix
@@ -1,4 +1,11 @@
 {lib, ...}: {
   imports = [../../home];
   nixpkgs.overlays = lib.mkForce null;
+  home-modules.development = {
+    enable = true;
+    languages = {
+      data.enable = true;
+      javascript.enable = true;
+    };
+  };
 }
