1588fcf4e00efbf634b921f2377c6ccc285a1639
0xlua <dev@lukasjordan.com>
2026-01-01 18:13:58 +0100

    feat: mount nas storage in ganymede

--- hosts/ganymede/default.nix
+++ hosts/ganymede/default.nix
@@ -1,10 +1,12 @@
 {
   pkgs,
   inputs,
+  config,
   ...
 }: {
   imports = [
     inputs.home-manager.nixosModules.home-manager
+    inputs.sops-nix.nixosModules.sops
     ./hardware-configuration.nix
   ];
 
@@ -39,6 +41,42 @@
       UseDns = true;
       PermitRootLogin = "no";
     };
+  };
+
+  sops = {
+    secrets = {
+      "nas/username_server" = {};
+      "nas/domain" = {};
+      "nas/password" = {};
+    };
+    templates = {
+      "smb-secret-server" = {
+        owner = "lua";
+        content = ''
+          username=${config.sops.placeholder."nas/username_server"}
+          domain=${config.sops.placeholder."nas/domain"}
+          password=${config.sops.placeholder."nas/password"}
+        '';
+      };
+    };
+  };
+
+  environment.systemPackages = [pkgs.cifs-utils];
+
+  fileSystems."/home/lua/media" = {
+    device = "//io.internal/media";
+    fsType = "cifs";
+    options = let
+      automount_opts = "nobrl,x-systemd.automount,x-systemd.requires=network-online.target";
+    in ["${automount_opts},credentials=${config.sops.templates."smb-secret-server".path},uid=1000,gid=100"];
+  };
+
+  fileSystems."/home/lua/scanner" = {
+    device = "//io.internal/scanner";
+    fsType = "cifs";
+    options = let
+      automount_opts = "nobrl,x-systemd.automount,x-systemd.requires=network-online.target";
+    in ["${automount_opts},credentials=${config.sops.templates."smb-secret-server".path},uid=1000,gid=100"];
   };
 
   system.stateVersion = "25.11"; # Don't change
