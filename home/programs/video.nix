9a606dbd9aab78a2ddd84a5267df02512b0a8a35
0xlua <dev@lukasjordan.com>
2026-01-28 08:00:31 +0100

    chore: remove freetube
    rely on mpv for youtube videos

--- home/programs/video.nix
+++ home/programs/video.nix
@@ -9,21 +9,6 @@
 
     programs.yt-dlp.enable = true;
 
-    programs.freetube = {
-      enable = true;
-      settings = {
-        checkForUpdates = false;
-        baseTheme = "nordic";
-        defaultTheatreMode = true;
-        defaultQuality = "1080";
-        # allowDashAv1Formats = true;
-        hideTrendingVideos = true;
-        hidePopularVideos = true;
-        hideSubscriptionsLive = true;
-        hideSubscriptionsShorts = true;
-      };
-    };
-
     programs.mpv.enable = true;
   };
 }
