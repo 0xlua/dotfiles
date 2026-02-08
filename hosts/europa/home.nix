3f0efe4316c377fd670e449a344500baee1faa2a
0xlua <dev@lukasjordan.com>
2026-02-08 21:33:41 +0100

    chore: enable dev tools on europa

--- hosts/europa/home.nix
+++ hosts/europa/home.nix
@@ -3,4 +3,15 @@
   nixpkgs.overlays = lib.mkForce null;
   home-modules.desktop.enable = true;
   home-modules.desktop.compositor = "niri";
+  home-modules.development = {
+    enable = true;
+    languages = {
+      android.enable = true;
+      rust.enable = true;
+      python.enable = true;
+      javascript.enable = true;
+      typesetting.enable = true;
+      data.enable = true;
+    };
+  };
 }
