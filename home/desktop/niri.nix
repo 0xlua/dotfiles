79491522e14b13c468b629328b62329a62e2c81a
0xlua <dev@lukasjordan.com>
2026-03-17 20:40:47 +0100

    feat(ironbar): migrate from nord theme to builtin minimal theme

--- home/desktop/niri.nix
+++ home/desktop/niri.nix
@@ -153,12 +153,8 @@ in {
       package = pkgs.ironbar;
       config = {
         position = "top";
+        height = 24;
         layer = "overlay";
-        margin = {
-          top = 8;
-          left = 16;
-          right = 16;
-        };
         start = [
           {
             type = "workspaces";
@@ -172,8 +168,11 @@ in {
         ];
         end = [
           {
+            type = "tray";
+          }
+          {
             type = "network_manager";
-            icon_size = 32;
+            types_blacklist = ["loopback" "bridge"];
           }
           {
             type = "volume";
@@ -189,51 +188,102 @@ in {
         ];
       };
       style = "
-      @define-color nord0 #2e3440;
-      @define-color nord1 #3b4252;
-      @define-color nord2 #434c5e;
-      @define-color nord3 #4c566a;
-      @define-color nord4 #d8dee9;
-      @define-color nord5 #e5e9f0;
-      @define-color nord6 #eceff4;
-      @define-color nord8 #88c0d0;
-      @define-color nord9 #81a1c1;
-      @define-color nord10 #5e81ac;
-      @define-color nord11 #bf616a;
-      @define-color nord12 #d08770;
-      @define-color nord13 #ebcb8b;
-      @define-color nord14 #a3be8c;
-      @define-color nord15 #b48ead;
+        :root {
+            --color-dark-primary: #1c1c1c;
+            --color-dark-secondary: #2d2d2d;
+            --color-white: #fff;
+            --color-active: #6699cc;
+            --color-urgent: #8f0a0a;
 
-      * {
-        font-family: 'Inter Display Medium';
-        font-size: 18px;
-        border: none;
-        border-radius: 12px;
-        background-image: none;
-        box-shadow: none;
-      }
+            --margin-lg: 1em;
+            --margin-sm: 0.5em;
+        }
 
-      .background {
-          background-color: transparent;
-      }
+        * {
+            border-radius: 0;
+            border: none;
+            box-shadow: none;
+            background-image: none;
+            font-family: monospace;
+        }
 
-      box, menubar, button, label {
-        background-color: @nord1;
-        color: @nord6;
-        padding: 2px;
-      }
+        scale > trough {
+            background-color: var(--color-dark-secondary);
+        }
 
-      .widget {
-        margin: 4px;
-      }
+        scale > trough > highlight {
+            background-color: var(--color-active);
+            border-style: solid;
+            border-color: var(--color-active);
+            border-width: 0.2em;
+        }
 
-      .workspaces .item.focused {
-        color: @nord1;
-        background-color: @nord6;
-        border-radius: 32px;
-      }
-    ";
+        scale > trough > slider {
+            background-color: var(--color-white);
+        }
+
+        switch > slider {
+            background-color: var(--color-white);
+        }
+
+        switch:checked {
+            background-color: var(--color-active);
+        }
+
+        switch:not(:checked) {
+          background-color: var(--color-dark-secondary);
+        }
+
+        #bar, popover, popover contents, calendar {
+            background-color: var(--color-dark-primary);
+        }
+
+        box, button, label {
+            background-color: #0000;
+            color: var(--color-white);
+        }
+
+        button {
+            padding-left: var(--margin-sm);
+            padding-right: var(--margin-sm);
+        }
+
+        button:hover, button:active {
+            background-color: var(--color-dark-secondary);
+        }
+
+        #end > * + * {
+            margin-left: var(--margin-lg);
+        }
+
+        .sysinfo > * + * {
+            margin-left: var(--margin-sm);
+        }
+
+        .clock {
+            font-weight: bold;
+        }
+
+        .popup-clock .calendar-clock {
+            font-size: 2.0em;
+        }
+
+        .popup-clock .calendar .today {
+            background-color: var(--color-active);
+        }
+
+        .workspaces .item.visible {
+            box-shadow: inset 0 -1px var(--color-white);
+        }
+
+        .workspaces .item.focused {
+            box-shadow: inset 0 -1px var(--color-active);
+            background-color: var(--color-dark-secondary);
+        }
+
+        .workspaces .item.urgent {
+            background-color: var(--color-urgent);
+        }";
     };
 
     services.kanshi = {
