2d2383baada05ce402a2b75e110c03bc787863ac
0xlua <dev@lukasjordan.com>
2026-04-30 10:43:48 +0200

    feat(zed): add extensions & lsp config, also enable helix mode

--- home/editors/zed.nix
+++ home/editors/zed.nix
@@ -1,7 +1,7 @@
 {config, ...}: {
   programs.zed-editor = {
     enable = config.home-modules.desktop.enable;
-    extensions = ["nix"];
+    extensions = ["deno" "biome" "nix" "sql" "typst" "html" "markdown-oxide" "latex"];
     userSettings = {
       auto_update = false;
       disable_ai = true;
@@ -9,14 +9,34 @@
         diagnostics = false;
         metrics = false;
       };
+      lsp = {
+        texlab.settings = config.programs.helix.languages.language-server.texlab.config;
+        tinymist.settings = {
+          exportPdf = "onSave";
+          outputPath = "$root/$name";
+        };
+        deno.settings = config.programs.helix.languages.language-server.deno-lsp.config;
+      };
       languages = {
-        Nix.formatter.external = {
-          command = "alejandra";
-          arguments = ["--quiet" "--"];
+        Python = {
+          language_servers = ["ruff" "ty" "!basedpyright"];
+        };
+        Nix = {
+          language_servers = ["nil" "!nixd"];
+          formatter.external = {
+            command = "alejandra";
+            arguments = ["--quiet" "--"];
+          };
+        };
+        LaTeX = {
+          formatter.external = {
+            command = "tex-fmt";
+            arguments = ["--stdin"];
+          };
         };
       };
       use_smartcase_search = true;
-      vim_mode = true;
+      helix_mode = true;
     };
   };
 }
