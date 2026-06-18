6e7e4bbe9409f608954f71a1cc323617f43b8488
0xlua <dev@lukasjordan.com>
2026-06-18 14:19:58 +0200

    feat(llm): reenable local LLMs, but switch to containerized llamacpp

--- home/programs/llm.nix
+++ home/programs/llm.nix
@@ -6,12 +6,31 @@
 }: let
   cfg = config.home-modules.llm;
 in {
-  options.home-modules.llm.enable = lib.mkEnableOption "LLM Tools like Ollama";
+  options.home-modules.llm = {
+    enable = lib.mkEnableOption "LLM Tools like Ollama";
+    modelPath = lib.mkOption {default = "${config.home.homeDirectory}/Downloads/models";};
+    model = lib.mkOption {default = "Qwen3.5-9B-Q4_K_M";};
+  };
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [pi-coding-agent];
-    services.ollama = {
+    # home.packages = with pkgs; [pi-coding-agent];
+    services.podman = {
       enable = true;
-      acceleration = "rocm";
+      # https://github.com/ggml-org/llama.cpp/blob/master/docs/docker.md
+      containers = {
+        "llamacpp" = {
+          image = "ghcr.io/ggml-org/llama.cpp:server-rocm";
+          autoStart = false;
+          volumes = ["${cfg.modelPath}:/models"];
+          ports = ["8080:8080"];
+          autoUpdate = "registry";
+          devices = ["/dev/kfd:/dev/kfd" "/dev/dri:/dev/dri"];
+          environment = {
+            LLAMA_ARG_MODEL = "/models/${cfg.model}.gguf";
+            # LLAMA_ARG_MODELS_DIR = "/models";
+            LLAMA_ARG_N_PREDICT = "512";
+          };
+        };
+      };
     };
   };
 }
