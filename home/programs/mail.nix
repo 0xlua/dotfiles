0bb0919a1eecd8a3fad5d255decb32bcb8e2a3f4
0xlua <dev@lukasjordan.com>
2026-07-10 18:28:26 +0200

    feat(aerc): complete config (see #134)

--- home/programs/mail.nix
+++ home/programs/mail.nix
@@ -12,62 +12,104 @@ in {
   ];
   options.home-modules.mail.enable = lib.mkEnableOption "mail";
 
-  config = lib.mkIf cfg.enable {
-    programs.aerc = {
-      enable = true;
-      extraConfig.general.unsafe-accounts-conf = true;
-    };
-    programs.thunderbird = {
-      inherit (config.home-modules.desktop) enable;
-      package = pkgs.thunderbird-latest;
-      profiles.lua = {
-        isDefault = true;
-        settings = {
-          "calendar.week.start" = 1;
-        };
-      };
-    };
-    sops.secrets."mailAppPw".mode = "0440";
-    accounts.calendar.accounts.lua.remote = {
-      type = "caldav";
-      url = "https://mail.lua.one/.well-known/caldav";
-      userName = "moin@lua.one";
-      passwordCommand = "cat ${config.sops.secrets."mailAppPw".path}";
-    };
-    accounts.email.accounts.lua = {
-      address = "moin@lua.one";
-      userName = "moin@lua.one";
-      primary = true;
-      realName = "Lukas Jordan";
-      imap = {
-        host = "mail.lua.one";
-        port = 993;
-        tls.enable = true;
-      };
-      smtp = {
-        host = "mail.lua.one";
-        port = 465;
-        tls.enable = true;
-      };
-      passwordCommand = "cat ${config.sops.secrets."mailAppPw".path}";
-      thunderbird = {inherit (config.home-modules.desktop) enable;};
-      aerc = {
+  config = let
+    email = "moin@lua.one";
+    host = "mail.lua.one";
+    passwordCommand = "cat ${config.sops.secrets."stalwart_app_token".path}";
+  in
+    lib.mkIf cfg.enable {
+      sops.secrets."stalwart_app_token".mode = "0440";
+
+      programs.gpg = {
         enable = true;
-        imapAuth = "oauthbearer";
-        imapOauth2Params = {
-          client_id = "mail";
-          client_secret = "TODO";
-          scope = "openid profile email";
-          token_endpoint = "https://auth.lua.one/auth/v1/oidc/token";
+        settings = {
+          encrypt-to = email;
         };
-        smtpAuth = "oauthbearer";
-        smtpOauth2Params = {
-          client_id = "mail";
-          client_secret = "TODO";
-          scope = "openid profile email";
-          token_endpoint = "https://auth.lua.one/auth/v1/oidc/token";
+      };
+
+      services.gpg-agent = {
+        enable = true;
+        pinentry.package = pkgs.pinentry-tty;
+      };
+
+      programs.aerc = {
+        enable = true;
+        extraConfig = {
+          hooks = {
+            mail-received = "notify-send \"New mail from $AERC_FROM_NAME\" \"$AERC_SUBJECT\"";
+          };
+          filters = {
+            "text/plain" = "colorize";
+            "text/html" = "html | colorize";
+          };
+          general = {
+            unsafe-accounts-conf = true;
+            pgp-provider = "gpg";
+            use-terminal-pinentry = true;
+          };
+        };
+      };
+
+      programs.thunderbird = {
+        inherit (config.home-modules.desktop) enable;
+        package = pkgs.thunderbird-latest;
+        profiles.lua = {
+          isDefault = true;
+          settings = {
+            "calendar.week.start" = 1;
+          };
+        };
+      };
+
+      accounts = {
+        calendar.accounts.lua.remote = {
+          type = "caldav";
+          url = "https://${host}/.well-known/caldav";
+          userName = email;
+          inherit passwordCommand;
+        };
+        email.accounts.lua = {
+          address = email;
+          userName = email;
+          inherit passwordCommand;
+          primary = true;
+          realName = "Lukas Jordan";
+          jmap.host = "${host}/.well-known/jmap";
+          gpg = {
+            key = "";
+            signByDefault = true;
+            encryptByDefault = true;
+          };
+          folders = {
+            inbox = "Inbox";
+            sent = "Sent Items";
+            drafts = "Drafts";
+            trash = "Deleted Items";
+          };
+          # imap = {
+          #   host = "mail.lua.one";
+          #   port = 993;
+          #   tls.enable = true;
+          # };
+          # smtp = {
+          #   host = "mail.lua.one";
+          #   port = 465;
+          #   tls.enable = true;
+          # };
+          # thunderbird = {inherit (config.home-modules.desktop) enable;};
+          aerc = {
+            enable = true;
+            extraAccounts = {
+              source = "jmap://${lib.strings.escapeURL email}@${host}";
+              source-cred-cmd = passwordCommand;
+              outgoing = "jmap://";
+              use-labels = true;
+              cache-state = true;
+              cache-blobs = false;
+              check-mail = "1m";
+            };
+          };
         };
       };
     };
-  };
 }
