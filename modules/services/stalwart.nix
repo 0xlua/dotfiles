1623b77657d1cf0f0d6461876bda69ce6b0c3c3a
0xlua <dev@lukasjordan.com>
2026-07-10 12:00:47 +0200

    chore(stalwart): update to v0.16 (closes #199)

--- modules/services/stalwart.nix
+++ modules/services/stalwart.nix
@@ -10,13 +10,12 @@ in {
   config = lib.mkIf cfg.enable {
     programs.rust-motd.settings.service_status.stalwart = config.virtualisation.oci-containers.containers.stalwart.serviceName;
     virtualisation.oci-containers.containers.stalwart = {
-      image = "docker.io/stalwartlabs/stalwart:v0.15";
-      labels = {
-        "io.containers.autoupdate" = "registry";
-      };
+      image = "docker.io/stalwartlabs/stalwart:v0.16";
+      labels."io.containers.autoupdate" = "registry";
       ports = ["25:25" "465:465" "993:993"];
       volumes = [
-        "/home/lua/podman/stalwart:/opt/stalwart"
+        "/home/lua/podman/stalwart-etc:/etc/stalwart:Z"
+        "/home/lua/podman/stalwart-data:/var/lib/stalwart:Z"
       ];
     };
   };
