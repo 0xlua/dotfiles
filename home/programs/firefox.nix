79b1ce309e1b5fb72a48538074b2b9b35c9350f6
0xlua <dev@lukasjordan.com>
2026-07-13 08:10:55 +0200

    feat(firefox): uBlock Origin Settings
    I also moved preferences to the default profile instead of a policy. This enables native messaging hosts again (see #170). The Policies were previously overwritten by Phonix

--- home/programs/firefox.nix
+++ home/programs/firefox.nix
@@ -2,6 +2,7 @@
   pkgs,
   inputs,
   config,
+  lib,
   ...
 }: {
   stylix.targets.firefox.profileNames = ["default"];
@@ -16,7 +17,6 @@
 
   programs.firefox = {
     inherit (config.home-modules.desktop) enable;
-    package = pkgs.wrapFirefox (pkgs.firefox-unwrapped.override {pipewireSupport = true;}) {};
     configPath = "${config.xdg.configHome}/mozilla/firefox";
     nativeMessagingHosts = [pkgs.tridactyl-native];
     policies = {
@@ -24,7 +24,43 @@
       SearchBar = "unified";
       TranslateEnabled = false;
       PictureInPicture.Enabled = false;
-      Preferences = {
+      "3rdparty".Extensions = {
+        "uBlock0@raymondhill.net".adminSettings = {
+          userSettings = rec {
+            advancedUserEnabled = true;
+            externalLists = lib.concatStringsSep "\n" importedLists;
+            firewallPaneMinimized = false;
+            importedLists = ["https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt"];
+            popupPanelSections = 31;
+          };
+          selectedFilterLists = [
+            "user-filters"
+            "ublock-filters"
+            "ublock-badware"
+            "ublock-privacy"
+            "ublock-quick-fixes"
+            "ublock-unbreak"
+            "easylist"
+            "easyprivacy"
+            "adguard-spyware-url"
+            "urlhaus-1"
+            "plowe-0"
+            "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt"
+          ];
+          hostnameSwitchesString = lib.concatStringsSep "\n" [
+            "no-large-media: behind-the-scene false"
+            "no-csp-reports: * true"
+            "no-scripting: マリウス.com true"
+            "* * 3p-script block"
+            "* * 3p-frame block"
+          ];
+        };
+      };
+    };
+    globalExtensions = with inputs.firefox-addons.packages.x86_64-linux; [bitwarden ublock-origin linkding-extension tridactyl];
+    profiles.default = {
+      isDefault = true;
+      settings = {
         "widget.gtk.libadwaita-colors.enabled" = false; # allow native styling
         "layout.css.prefers-color-scheme.content-override" = 0; # dark mode
         "sidebar.verticalTabs" = true;
@@ -33,13 +69,8 @@
         "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned" = "";
         # re-enable native messaging (https://codeberg.org/celenity/Phoenix/commit/bb82bd987d8a5347d1d7213ffb8820298d51a52f)
         "webextensions.native-messaging.max-input-message-bytes" = 1048576;
-        "webextensions.native-messaging.max-output-message-bytes" = 999999999;
+        "webextensions.native-messaging.max-output-message-bytes" = 2147483647;
       };
-    };
-    globalExtensions = with inputs.firefox-addons.packages.x86_64-linux; [bitwarden ublock-origin linkding-extension tridactyl];
-    profiles.default = {
-      isDefault = true;
-      #extensions.settings."uBlock0@raymondhill.net" = {};
       search = {
         default = "ddg";
         force = true;
