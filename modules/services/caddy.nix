b0628d809dfa3122176658dd0bed8664138d218d
0xlua <dev@lukasjordan.com>
2026-07-17 17:44:37 +0200

    feat(caddy): container hardening (#168)

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -10,18 +10,25 @@ in {
   config = lib.mkIf cfg.enable {
     programs.rust-motd.settings.service_status.caddy = config.virtualisation.oci-containers.containers.caddy.serviceName;
     virtualisation.oci-containers.containers.caddy = {
-      image = "docker.io/caddy:latest";
+      image = "docker.io/caddy:2-alpine";
       user = "1000:100";
-      autoStart = true;
-      labels = {
-        "io.containers.autoupdate" = "registry";
+      capabilities = {
+        ALL = false;
+        NET_BIND_SERVICE = true;
+        NET_ADMIN = true; #optional: improves HTTP/3 performance
       };
+      extraOptions = [
+        "--read-only"
+        "--security-opt=no-new-privileges"
+      ];
+      autoStart = true;
+      labels."io.containers.autoupdate" = "registry";
       volumes = [
-        "/home/lua/podman/caddy_etc:/etc/caddy"
+        "${../../files/caddy}:/etc/caddy:ro"
         "/home/lua/podman/static:/srv"
         "/home/lua/podman/caddy:/data"
       ];
-      ports = ["80:80" "443:443"];
+      ports = ["80:80/tcp" "443:443/tcp" "443:443/udp"];
     };
   };
 }
