7be3f5dfb453ee9be180b91367f56c4e786b66d4
0xlua <dev@lukasjordan.com>
2026-07-18 16:13:58 +0200

    feat(stalwart): container hardeneing & use wildcard certificate (#168)

--- modules/services/stalwart.nix
+++ modules/services/stalwart.nix
@@ -11,12 +11,21 @@ in {
     programs.rust-motd.settings.service_status.stalwart = config.virtualisation.oci-containers.containers.stalwart.serviceName;
     virtualisation.oci-containers.containers.stalwart = {
       image = "docker.io/stalwartlabs/stalwart:v0.16";
+      autoStart = true;
       labels."io.containers.autoupdate" = "registry";
+      capabilities = {
+        ALL = false;
+        NET_BIND_SERVICE = true;
+      };
+      extraOptions = [
+        "--read-only"
+        "--security-opt=no-new-privileges"
+      ];
       ports = ["25:25" "465:465" "993:993"];
       volumes = [
         "/home/lua/podman/stalwart-etc:/etc/stalwart:Z"
         "/home/lua/podman/stalwart-data:/var/lib/stalwart:Z"
-        "/home/lua/podman/caddy/caddy/certificates/acme-v02.api.letsencrypt.org-directory/mail.lua.one:/opt/certificates:ro,idmap=uids=1000-2000-1;gids=100-2000-1"
+        "/home/lua/podman/caddy/caddy/certificates/acme-v02.api.letsencrypt.org-directory/wildcard_.lua.one:/opt/certificates:ro,idmap=uids=1000-2000-1;gids=100-2000-1"
       ];
     };
   };
