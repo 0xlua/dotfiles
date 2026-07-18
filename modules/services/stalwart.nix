afdc1eb4ed305573d10481a41b7649e88182741d
0xlua <dev@lukasjordan.com>
2026-07-18 16:42:04 +0200

    feat(stalwart): move config inside flake (#168)

--- modules/services/stalwart.nix
+++ modules/services/stalwart.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfg = config.server.stalwart;
@@ -9,7 +10,12 @@ in {
 
   config = lib.mkIf cfg.enable {
     programs.rust-motd.settings.service_status.stalwart = config.virtualisation.oci-containers.containers.stalwart.serviceName;
-    virtualisation.oci-containers.containers.stalwart = {
+    virtualisation.oci-containers.containers.stalwart = let
+      stalwart_config = (pkgs.formats.json {}).generate "stalwart_config" {
+        "@type" = "RocksDb";
+        "path" = "/var/lib/stalwart";
+      };
+    in {
       image = "docker.io/stalwartlabs/stalwart:v0.16";
       autoStart = true;
       labels."io.containers.autoupdate" = "registry";
@@ -23,7 +29,7 @@ in {
       ];
       ports = ["25:25" "465:465" "993:993"];
       volumes = [
-        "/home/lua/podman/stalwart-etc:/etc/stalwart:Z"
+        "${stalwart_config}:/etc/stalwart/config.json:ro"
         "/home/lua/podman/stalwart-data:/var/lib/stalwart:Z"
         "/home/lua/podman/caddy/caddy/certificates/acme-v02.api.letsencrypt.org-directory/wildcard_.lua.one:/opt/certificates:ro,idmap=uids=1000-2000-1;gids=100-2000-1"
       ];
