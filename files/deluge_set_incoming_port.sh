cfdd9ea9ec700ff82b0971d2648ddc8fa0978e32
0xlua <dev@lukasjordan.com>
2026-07-22 09:16:30 +0200

    fix(gluetun): retry port forward script until deluge is reachable

--- files/deluge_set_incoming_port.sh
+++ files/deluge_set_incoming_port.sh
@@ -5,7 +5,7 @@ url="http://deluge:8112/json"
 password="$(cat  /run/secrets/deluge_password)"
  
 json="{\"method\": \"auth.login\", \"params\": [\"$password\"], \"id\": \"1\"}"
-wget -O- -q --header="$header" --post-data="$json" --save-cookies cookies.txt $url
+wget -O- -q --tries=20 --waitretry=30s --retry-connrefused --header="$header" --post-data="$json" --save-cookies cookies.txt $url
 
 json="{\"method\": \"core.set_config\", \"params\": [{\"listen_ports\": [$1, $1]}], \"id\": \"2\"}"
 wget -O- -q --header="$header" --post-data="$json" --load-cookies cookies.txt $url
