287ff26ed2c78267973d2ae50326ac28565a1cb5
0xlua <dev@lukasjordan.com>
2026-07-28 08:45:38 +0200

    feat(gpg): add my public keys via nix

--- home/programs/mail.nix
+++ home/programs/mail.nix
@@ -24,6 +24,14 @@ in {
       programs.gpg = {
         enable = true;
         settings.encrypt-to = email;
+        publicKeys = let
+          trust = 5;
+        in
+          map (source: {inherit source trust;}) [
+            ../../files/certs/gpg/lukasjordan.com/hu/aeii9rmagouy1owpp7e5ftpxjof7h41n # hi@lukasjordan.com
+            ../../files/certs/gpg/lukasjordan.com/hu/ze6x9uruirzyt6bcgxni5ndsc569g3fq # moin@lukasjordan.com
+            ../../files/certs/gpg/lua.one/hu/ze6x9uruirzyt6bcgxni5ndsc569g3fq # moin@lua.one
+          ];
       };
 
       services.gpg-agent = {
