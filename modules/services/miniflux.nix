78eae6ec6c45fb305fbde28b2dacb81d6e6ea1e0
0xlua <dev@lukasjordan.com>
2026-07-29 12:06:35 +0200

    feat(miniflux): healthcheck & update postgres (#168)

--- modules/services/miniflux.nix
+++ modules/services/miniflux.nix
@@ -27,6 +27,14 @@ in {
         image = "ghcr.io/miniflux/miniflux:latest";
         autoStart = true;
         dependsOn = ["miniflux-db"];
+        capabilities.ALL = false;
+        extraOptions = [
+          "--health-cmd=[\"/usr/bin/miniflux\", \"-healthcheck\", \"auto\"]"
+          "--health-interval=30s"
+          "--health-timeout=10s"
+          "--read-only"
+          "--security-opt=no-new-privileges"
+        ];
         labels."io.containers.autoupdate" = "registry";
         environmentFiles = [
           config.sops.templates."miniflux-env".path
@@ -43,22 +51,39 @@ in {
           FETCHER_ALLOW_PRIVATE_NETWORKS = "1";
         };
         volumes = [
-          "${config.sops.secrets."miniflux/oidc_client_secret".path}:/run/secrets/oidc_client_secret"
-          "${config.sops.secrets."miniflux/oidc_client_id".path}:/run/secrets/oidc_client_id"
+          "${config.sops.secrets."miniflux/oidc_client_secret".path}:/run/secrets/oidc_client_secret:ro"
+          "${config.sops.secrets."miniflux/oidc_client_id".path}:/run/secrets/oidc_client_id:ro"
         ];
       };
       miniflux-db = {
-        image = "docker.io/postgres:17-alpine";
+        image = "docker.io/postgres:18-alpine";
+        user = "70";
+        autoStart = true;
+        capabilities = {
+          ALL = false;
+          CHOWN = true;
+          SETGID = true;
+          SETUID = true;
+          DAC_OVERRIDE = true;
+        };
+        extraOptions = [
+          "--health-cmd=[\"pg_isready\"]"
+          "--health-interval=10s"
+          "--health-start-period=30s"
+          "--read-only"
+          "--security-opt=no-new-privileges"
+        ];
+        labels."io.containers.autoupdate" = "registry";
         environment = {
           POSTGRES_USER_FILE = "/run/secrets/user";
           POSTGRES_PASSWORD_FILE = "/run/secrets/password";
           POSTGRES_DB_FILE = "/run/secrets/db";
         };
         volumes = [
-          "/home/lua/podman/miniflux:/var/lib/postgresql/data"
-          "${config.sops.secrets."miniflux/password".path}:/run/secrets/password"
-          "${config.sops.secrets."miniflux/user".path}:/run/secrets/user"
-          "${config.sops.secrets."miniflux/db".path}:/run/secrets/db"
+          "/home/lua/podman/miniflux:/var/lib/postgresql"
+          "${config.sops.secrets."miniflux/password".path}:/run/secrets/password:ro"
+          "${config.sops.secrets."miniflux/user".path}:/run/secrets/user:ro"
+          "${config.sops.secrets."miniflux/db".path}:/run/secrets/db:ro"
         ];
       };
     };
