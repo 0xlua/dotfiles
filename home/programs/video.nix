9af33571de635400a77ca36830e8f2e67ecb9888
0xlua <dev@lukasjordan.com>
2026-08-03 09:58:35 +0200

    feat(mpv): add mpris, thumbfast and modernz scripts

--- home/programs/video.nix
+++ home/programs/video.nix
@@ -5,10 +5,16 @@
   ...
 }: {
   config = lib.mkIf config.home-modules.desktop.enable {
-    home.packages = with pkgs; [delfin youtube-tui];
+    home.packages = with pkgs; [
+      delfin # jellyfin
+      youtube-tui # youtube
+    ];
 
     programs.yt-dlp.enable = true;
 
-    programs.mpv.enable = true;
+    programs.mpv = {
+      enable = true;
+      package = pkgs.mpv.override {scripts = with pkgs.mpvScripts; [mpris thumbfast modernz];};
+    };
   };
 }
