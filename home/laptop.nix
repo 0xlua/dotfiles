0b6f742ab336a0cae14d01ae6dd19d6898832e17
0xlua <dev@lukasjordan.com>
2026-08-05 12:44:40 +0200

    feat(home): add bluetui & mpris-proxy

--- home/laptop.nix
+++ home/laptop.nix
@@ -1,43 +1,48 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: let
   cfg = config.home-modules.laptop;
 in {
   options.home-modules.laptop.enable = lib.mkEnableOption "Laptop tools";
   config = lib.mkIf cfg.enable {
-    services.kanshi = {
-      enable = true;
-      settings = [
-        {
-          profile.name = "undocked";
-          profile.exec = ["wpaperctl reload"];
-          profile.outputs = [
-            {
-              criteria = "eDP-1";
-              status = "enable";
-              mode = "1920x1080@60.049";
-              scale = 1.0;
-            }
-          ];
-        }
-        {
-          profile.name = "docked";
-          profile.exec = ["wpaperctl reload"];
-          profile.outputs = [
-            {
-              criteria = "eDP-1";
-              status = "disable";
-            }
-            {
-              criteria = "Philips Consumer Electronics Company 34M2C6500 AU42423000414";
-              position = "0,0";
-              scale = 1.0;
-            }
-          ];
-        }
-      ];
+    home.packages = with pkgs; [bluetui];
+    services = {
+      mpris-proxy.enable = true;
+      kanshi = {
+        enable = true;
+        settings = [
+          {
+            profile.name = "undocked";
+            profile.exec = ["wpaperctl reload"];
+            profile.outputs = [
+              {
+                criteria = "eDP-1";
+                status = "enable";
+                mode = "1920x1080@60.049";
+                scale = 1.0;
+              }
+            ];
+          }
+          {
+            profile.name = "docked";
+            profile.exec = ["wpaperctl reload"];
+            profile.outputs = [
+              {
+                criteria = "eDP-1";
+                status = "disable";
+              }
+              {
+                criteria = "Philips Consumer Electronics Company 34M2C6500 AU42423000414";
+                position = "0,0";
+                scale = 1.0;
+              }
+            ];
+          }
+        ];
+      };
     };
   };
 }
