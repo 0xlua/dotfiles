dbeefb7d2eed2ea1f21d89a039ca85c81e68775a
0xlua <dev@lukasjordan.com>
2026-08-18 08:17:41 +0200

    feat(paperless): update to 3.0

--- modules/roles/server/paperless.nix
+++ modules/roles/server/paperless.nix
@@ -8,18 +8,26 @@ in {
   options.modules.roles.server.paperless.enable = lib.mkEnableOption "paperless-ngx";
 
   config = lib.mkIf cfg.enable {
+    programs.rust-motd.settings.service_status.paperless = config.virtualisation.oci-containers.containers.paperless.serviceName;
+
+    sops = {
+      secrets."paperless/secret_key" = {};
+      templates."paperless-env".content = "PAPERLESS_SECRET_KEY=${config.sops.placeholder."paperless/secret_key"}";
+    };
+
     virtualisation.oci-containers.containers = {
       paperless = {
-        image = "ghcr.io/paperless-ngx/paperless-ngx:latest";
+        image = "ghcr.io/paperless-ngx/paperless-ngx:3.0";
         autoStart = true;
         dependsOn = ["redis"];
         labels."io.containers.autoupdate" = "registry";
+        environmentFiles = [config.sops.templates."paperless-env".path];
         environment = {
           PAPERLESS_TIME_ZONE = "Europe/Berlin";
           USERMAP_UID = "1000";
           USERMAP_GID = "100";
           PAPERLESS_REDIS = "redis://redis:6379";
-          PAPERLESS_CONSUMER_POLLING = "10";
+          PAPERLESS_CONSUMER_POLLING_INTERVAL = "10";
         };
         ports = ["4000:8000"];
         volumes = [
